8a75093cbb3f7a9cfaccd739599c2741ad8de3c4
Carmine Perna <carmine.pernagutierrez@he-arc.ch>
2019-03-31 21:55:06 +0200

    Rest service "Lister tous les clients" 	(GET /customer) implemented and tested All test always passed

--- src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
+++ src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
@@ -4,7 +4,27 @@
 
 package ch.hearc.ig.odi.minishop.restresources;
 
+import ch.hearc.ig.odi.minishop.business.Customer;
+import ch.hearc.ig.odi.minishop.services.PersistenceService;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+
+@Path("customer")
 public class CustomerResource {
+    @Inject
+    private PersistenceService persistenceService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public ArrayList<Customer> findAllCustomer() {
+        return persistenceService.getAllCustomers();
+    }
+
 
 }
 
