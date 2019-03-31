bf6518317b16037468b720edaa729f3e794b2a50
Carmine Perna <carmine.pernagutierrez@he-arc.ch>
2019-03-31 21:56:33 +0200

    Rest service "Lister les informations d’un client" GET /customer/{id} implemented and tested All test always passed

--- src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
+++ src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
@@ -5,11 +5,13 @@
 package ch.hearc.ig.odi.minishop.restresources;
 
 import ch.hearc.ig.odi.minishop.business.Customer;
+import ch.hearc.ig.odi.minishop.exception.CustomerException;
 import ch.hearc.ig.odi.minishop.services.PersistenceService;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
@@ -25,6 +27,17 @@ public class CustomerResource {
         return persistenceService.getAllCustomers();
     }
 
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Customer getCustomerByID(@PathParam("id") Long id) throws CustomerException {
+        try{
+            return persistenceService.getCustomerByID(id);
+        }catch (CustomerException ex){
+            ex.printStackTrace();
+            throw new CustomerException("Customer with id " + id + " not found");
+        }
 
+    }
 }
 
